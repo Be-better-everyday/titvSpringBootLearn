26dded561b31d99883a4c263cb470e6c05cea523
Be-better-everyday <125204550+Be-better-everyday@users.noreply.github.com>
2023-09-06 08:48:50 +0700

    renew

---
+++ Projects/Session10_05Thymeleaf/src/main/java/com/titv/spring/thymleaf/Session1005ThymleafApplication.java
@@ -0,0 +1,13 @@
+package com.titv.spring.thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session1005ThymleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session1005ThymleafApplication.class, args);
+    }
+
+}
